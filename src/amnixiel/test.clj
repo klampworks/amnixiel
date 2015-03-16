457f407646b0c0eb98f81618051e95706f95a05a
yukipyon <wolfvbear@gmail.com>
2015-03-16 08:21:17 +0000

    Disabled DTD parsing.

--- src/amnixiel/test.clj
+++ src/amnixiel/test.clj
@@ -1,12 +1,22 @@
-(ns amnixiel.test)
+(ns amnixiel.test
+    (:import (javax.xml.parsers SAXParser SAXParserFactory)))
+
 (require '[clojure.java.io :as io])
 (require '[clojure.xml :as xml])
 (require '[clojure.zip :as zip])
 (require '[clojure.data.zip.xml :as zip-xml])
 
+(defn startparse-sax
+    "Skip DTDs."
+    [s ch]
+    (let [factory (SAXParserFactory/newInstance)]
+        (.setFeature factory "http://apache.org/xml/features/nonvalidating/load-external-dtd" false)
+        (let [^SAXParser parser (.newSAXParser factory)]
+            (.parse parser s ch))))
+
 (defn main []
     (def root (-> "example.nzb" io/resource io/file 
-               xml/parse zip/xml-zip))
+               (xml/parse startparse-sax) zip/xml-zip))
     (into {}
           (for [m (zip-xml/xml-> root :head :meta)]
                   [(keyword (zip-xml/attr m :type))

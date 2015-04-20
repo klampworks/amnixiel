2ec5b6da9b2ae04061595ec4c57f9828eeddff00
yukipyon <wolfvbear@gmail.com>
2015-04-20 10:33:20 +0100

    ...and cleaned out the main namespace.

--- src/amnixiel/test.clj
+++ src/amnixiel/test.clj
@@ -17,53 +17,15 @@
                        (element :baz {} "The baz value2")
                        (element :baz {} "The baz value3"))))
 
-(defn mkdesc-content [n]
-    (element :div {}
-        (element :p {:style "font-size:8pt;font-family:monospace;"} 
-            (<< "(~(n :lon),~(n :lat))"))
-        (element :ul {}
-            (element :li {} (<< "BSSID : ~(n :bssid)"))
-            (element :li {} (<< "Channel : ~(n :channel)"))
-            (element :li {} (<< "Encrypt : ~(clojure.string/join \\space (n :encryption))")))))
-           
-(defn mkdesc [n]
-    (sexp-as-element [:description {} 
-        [:-cdata (emitter/strip-meta (emit-str (mkdesc-content n)))]]))
-
-(defn network->kml [n]
-    (element :Placemark {}
-        (mkdesc n)
-        (element :name {} (n :essid))
-        (element :Point {}
-            (element :extrude {} "1")
-            (element :alitiudeMode {} "relativeToGround")
-            (element :coordinates {} (<< "~(n :lon),~(n :lat),0")))
-        (element :styleUrl {} 
-            (<< "#~(colours/colour->id (colours/pick-colour (n :encryption)))"))))
-
 ;(print (main "test.xml"))
 
 ;(print (indent-str (first (main "test.xml"))))
 
-(defn style->kml [c]
-    (element :style {:id (colours/colour->id c)}
-        (element :LabelStyle {}
-            (element :color {} (colours/colour->hex c)))))
-
-(defn kml [n]
-    (def root (zip/xml-zip (element :Document {}
-            (style->kml :red)
-            (style->kml :orange)
-            (style->kml :green)
-            (style->kml :black))))
-    (defn app [e] (zip/append-child root e))
-        (reduce #(
-           zip/append-child %1 (network->kml %2)) root (remove nil? n)))
 
 (defn main [f]
     (def root (-> f io/resource io/file 
                (xml/parse parser/startparse-sax) zip/xml-zip))
-    (print (indent-str (first (kml (parser/parse-networks root))))))
+    (print (indent-str (first (emitter/kml (parser/parse-networks root))))))
     ;(parse-networks root))
 
 ;(print (indent-str (main "test-mini.xml")))

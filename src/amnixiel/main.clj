0854b61de0b0cfa405952ea6e4a02fa89774662a
yukipyon <wolfvbear@gmail.com>
2015-04-20 15:19:32 +0100

    Multiple command line arguments sort of.

--- src/amnixiel/main.clj
+++ src/amnixiel/main.clj
@@ -5,8 +5,12 @@
               [amnixiel.emitter :as emitter]
               [clojure.zip :as zip]))
 
-(defn main [f]
-    (def root (-> f io/resource io/file 
-               (xml/parse parser/startparse-sax) zip/xml-zip))
-    (print (clojure.data.xml/indent-str 
-        (first (emitter/kml (parser/parse-networks root))))))
+(defn main [& [f]]
+    (print 
+        (emitter/kml (map #(parser/parse-networks 
+                (-> % io/resource io/file 
+                  (xml/parse parser/startparse-sax) zip/xml-zip)) 
+             *command-line-args*))))
+
+    ;(print (clojure.data.xml/indent-str 
+    ;    (first (emitter/kml (parser/parse-networks root))))))

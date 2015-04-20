a761df324ad990b3d502e58b835bbdd3275b9fea
yukipyon <wolfvbear@gmail.com>
2015-04-20 10:39:35 +0100

    Cleaned up includes.

--- src/amnixiel/test.clj
+++ src/amnixiel/test.clj
@@ -1,17 +1,12 @@
 (ns amnixiel.test
-    (:import (javax.xml.parsers SAXParser SAXParserFactory))
     (:require [clojure.java.io :as io]
               [clojure.xml :as xml]
-              [amnixiel.colours :as colours]
               [amnixiel.parser :as parser]
               [amnixiel.emitter :as emitter]
-              [clojure.zip :as zip])
-    (:use
-              [clojure.core.strint]
-              [clojure.data.xml]
-              [clojure.data.zip.xml]))
+              [clojure.zip :as zip]))
 
 (defn main [f]
     (def root (-> f io/resource io/file 
                (xml/parse parser/startparse-sax) zip/xml-zip))
-    (print (indent-str (first (emitter/kml (parser/parse-networks root))))))
+    (print (clojure.data.xml/indent-str 
+        (first (emitter/kml (parser/parse-networks root))))))

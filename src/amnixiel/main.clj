51ed23b67427d387e52f1acc63b0702ea53be1b2
yukipyon <wolfvbear@gmail.com>
2015-05-04 07:00:18 +0100

    Added exception handling on unparsable files.

--- src/amnixiel/main.clj
+++ src/amnixiel/main.clj
@@ -4,6 +4,7 @@
               [amnixiel.parser :as parser]
               [amnixiel.emitter :as emitter]
               [clojure.zip :as zip]))
+
 (defn open [fs]
     (let [r (group-by #(.exists %) (map io/file fs))]
         (doseq [dne (r false)]
@@ -11,11 +12,15 @@
                 (println "File " (.getName dne) " does not exist. Skipping...")))
         (r true)))
 
+(defn parse [f]
+    (try  (-> f 
+            (xml/parse parser/startparse-sax) zip/xml-zip)
+        (catch Exception ex nil)))
+
 (defn main [& [f]]
     (print (clojure.data.xml/indent-str (first
         (emitter/kml 
             (reduce #(concat %1
-                (parser/parse-networks 
-                (-> %2 
-                  (xml/parse parser/startparse-sax) zip/xml-zip)))
+                (if-let [f (parse %2)]
+                    (parser/parse-networks f)))
              '() (open *command-line-args*)))))))

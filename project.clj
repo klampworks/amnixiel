db7d122390d3b41220894d157d8f84c9b4dc9767
yukipyon <wolfvbear@gmail.com>
2015-05-09 19:21:28 +0100

    Added uberjar configuration.

--- project.clj
+++ project.clj
@@ -7,4 +7,6 @@
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/core.incubator "0.1.3"]
                  [org.clojure/data.zip "0.1.1"]]
-  :main amnixiel.main/main)
+  :uberjar {:aot :all}
+  :aot [amnixiel.main]
+  :main amnixiel.main)
